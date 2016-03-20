606e860ebec34e9bd13ddbf21593b1b426073fb6
Maksym Savisko <stowyn@gmail.com>
2016-03-20 13:39:32 +0200

    Init

---
+++ Randomize Me/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Randomize Me
+//
+//  Created by Maksym Savisko on 3/20/16.
+//  Copyright © 2016 Maksym Savisko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Randomize Me/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Randomize Me
+//
+//  Created by Maksym Savisko on 3/20/16.
+//  Copyright © 2016 Maksym Savisko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
