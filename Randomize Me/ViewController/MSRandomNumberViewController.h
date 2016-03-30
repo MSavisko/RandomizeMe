6d5ab02e686bf7c238369241628a0ff206c9d55f
Maksym Savisko <stowyn@gmail.com>
2016-03-30 22:04:36 +0300

    Refactor viewcontroller

--- Randomize Me/ViewController/MSRandomNumberViewController.h
+++ Randomize Me/ViewController/MSRandomNumberViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface RandomNumberViewController : UIViewController
+@interface MSRandomNumberViewController : UIViewController
 
 @end
--- Randomize Me/ViewController/MSRandomNumberViewController.m
+++ Randomize Me/ViewController/MSRandomNumberViewController.m
@@ -6,12 +6,12 @@
 //  Copyright © 2016 Maksym Savisko. All rights reserved.
 //
 
-#import "RandomNumberViewController.h"
+#import "MSRandomNumberViewController.h"
 #import "MSRandomRequest.h"
 #import "MSRandomResponse.h"
 #import "AFNetworking.h"
 
-@interface RandomNumberViewController () <UITextFieldDelegate>
+@interface MSRandomNumberViewController () <UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UIButton *randomizeButton;
 @property (weak, nonatomic) IBOutlet UITextField *numberOfInteger;
 @property (weak, nonatomic) IBOutlet UITextField *minValue;
@@ -20,7 +20,7 @@
 @property (strong, nonatomic) MSRandomRequest *request;
 @end
 
-@implementation RandomNumberViewController
+@implementation MSRandomNumberViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
