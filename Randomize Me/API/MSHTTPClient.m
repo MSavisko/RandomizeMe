ad2fc5f80eb89c05ac20d71187094fd40bf0cc94
Maksym Savisko <stowyn@gmail.com>
2016-03-30 22:34:34 +0300

    init http client

---
+++ Randomize Me/API/MSHTTPClient.m
@@ -0,0 +1,16 @@
+//
+//  MSHttpClient.m
+//  Randomize Me
+//
+//  Created by Maksym Savisko on 3/30/16.
+//  Copyright © 2016 Maksym Savisko. All rights reserved.
+//
+
+#import "MSHTTPClient.h"
+
+@interface MSHTTPClient ()
+@end
+
+@implementation MSHTTPClient
+
+@end
---
+++ Randomize Me/API/MSHttpClient.h
@@ -0,0 +1,14 @@
+//
+//  MSHttpClient.h
+//  Randomize Me
+//
+//  Created by Maksym Savisko on 3/30/16.
+//  Copyright © 2016 Maksym Savisko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "AFNetworking.h"
+
+@interface MSHTTPClient : AFHTTPSessionManager
+
+@end
