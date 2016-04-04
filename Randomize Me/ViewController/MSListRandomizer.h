c2ed894db0a89580d4a4d9b1f3ca90ac503a52b1
Maksym Savisko <stowyn@gmail.com>
2016-04-04 12:36:56 +0300

    Init SWRealViewController

---
+++ Randomize Me/ViewController/MSListRandomizer.h
@@ -0,0 +1,13 @@
+//
+//  MSListRandomizer.h
+//  Randomize Me
+//
+//  Created by Maksym Savisko on 4/4/16.
+//  Copyright © 2016 Maksym Savisko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSListRandomizer : UIViewController
+
+@end
---
+++ Randomize Me/ViewController/MSListRandomizer.m
@@ -0,0 +1,13 @@
+//
+//  MSListRandomizer.m
+//  Randomize Me
+//
+//  Created by Maksym Savisko on 4/4/16.
+//  Copyright © 2016 Maksym Savisko. All rights reserved.
+//
+
+#import "MSListRandomizer.h"
+
+@implementation MSListRandomizer
+
+@end
