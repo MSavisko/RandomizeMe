fd6bffb9f20be7dbb4901c0c74181c07975a154d
Maksym Savisko <stowyn@gmail.com>
2016-04-05 10:02:08 +0300

    init new request model (integer)

---
+++ Randomize Me/Models/MSIntegerRequest.h
@@ -0,0 +1,13 @@
+//
+//  MSIntegerRequest.h
+//  Randomize Me
+//
+//  Created by Maksym Savisko on 4/5/16.
+//  Copyright © 2016 Maksym Savisko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MSIntegerRequest : NSObject
+
+@end
---
+++ Randomize Me/Models/MSIntegerRequest.m
@@ -0,0 +1,13 @@
+//
+//  MSIntegerRequest.m
+//  Randomize Me
+//
+//  Created by Maksym Savisko on 4/5/16.
+//  Copyright © 2016 Maksym Savisko. All rights reserved.
+//
+
+#import "MSIntegerRequest.h"
+
+@implementation MSIntegerRequest
+
+@end
