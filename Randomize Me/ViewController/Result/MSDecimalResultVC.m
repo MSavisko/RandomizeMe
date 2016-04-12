b6d7b2359943d56dfa11a1392da44ef5f886d05e
Maksym Savisko <stowyn@gmail.com>
2016-04-12 10:45:36 +0300

    Decimal Result VC: TrashButtonPressed added

--- Randomize Me/ViewController/Result/MSDecimalResultVC.m
+++ Randomize Me/ViewController/Result/MSDecimalResultVC.m
@@ -9,6 +9,8 @@
 #import "MSDecimalResultVC.h"
 #import "SWRevealViewController.h"
 
+#import "MBProgressHUD.h"
+
 @interface MSDecimalResultVC ()
 @property (weak, nonatomic) IBOutlet UITextView *resultTextView;
 @property (weak, nonatomic) IBOutlet UILabel *timestampLabel;
@@ -25,8 +27,8 @@
 - (void) viewDidLoad {
     [super viewDidLoad];
     [self hideKeyboardByTap];
-    [self.trashButton setEnabled:NO];
-    [self.trashButton setTintColor:[UIColor clearColor]];
+//    [self.trashButton setEnabled:NO];
+//    [self.trashButton setTintColor:[UIColor clearColor]];
     self.resultTextView.text = [self stringResultWithNumber:self.decimalPlaces];
     self.timestampLabel.text = [self stringComplitionTime];
 }
@@ -51,6 +53,12 @@
 }
 
 #pragma mark - IBAction
+- (IBAction)trashButtonPressed:(id)sender {
+    UIPasteboard *pb = [UIPasteboard generalPasteboard];
+    [pb setValue:@"" forPasteboardType:UIPasteboardNameGeneral];
+    [self showDeletingHud];
+    [self hideDeletingHud];
+}
 
 #pragma mark - Keyboard Methods
 - (void) hideKeyboardByTap {
@@ -63,6 +71,37 @@
 -(void) dismissKeyboard {
     [self.view endEditing:YES];
 }
+
+#pragma mark - MBProgressHUD Method
+- (void) showCopyingHud {
+    MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    hud.mode = MBProgressHUDModeCustomView;
+    hud.labelText = @"Сopied";
+}
+
+- (void) hideCopyingHud {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [MBProgressHUD hideAllHUDsForView:self.view animated:YES];
+        });
+    });
+}
+
+- (void) showDeletingHud {
+    MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    hud.mode = MBProgressHUDModeCustomView;
+    hud.labelText = @"Deleted";
+}
+
+- (void) hideDeletingHud {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [MBProgressHUD hideAllHUDsForView:self.view animated:YES];
+            [self.navigationController popViewControllerAnimated:YES];
+        });
+    });
+}
+
 
 #pragma mark - Presentation Data Method
 - (NSString*) stringResultWithNumber: (NSInteger)number {
