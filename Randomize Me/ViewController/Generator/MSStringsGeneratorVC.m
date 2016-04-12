bb5f5e4bc0a682814677b6e7962c920440e2ef02
Maksym Savisko <stowyn@gmail.com>
2016-04-12 16:16:55 +0300

    1. Init Strings Generator VC with menu; 2. Update for 8.1 SDK support

---
+++ Randomize Me/ViewController/Generator/MSStringsGeneratorVC.m
@@ -0,0 +1,13 @@
+//
+//  MSStringsGeneratorVC.m
+//  Randomize Me
+//
+//  Created by Maksym Savisko on 4/12/16.
+//  Copyright © 2016 Maksym Savisko. All rights reserved.
+//
+
+#import "MSStringsGeneratorVC.h"
+
+@implementation MSStringsGeneratorVC
+
+@end
