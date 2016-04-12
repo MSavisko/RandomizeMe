56232815671eddc3bbbbf71c778349a8044e6631
Maksym Savisko <stowyn@gmail.com>
2016-04-12 18:25:29 +0300

    Init Result Strings VC

---
+++ Randomize Me/ViewController/Result/MSStringsResultVC.m
@@ -0,0 +1,13 @@
+//
+//  MSStringsResultVC.m
+//  Randomize Me
+//
+//  Created by Maksym Savisko on 4/12/16.
+//  Copyright © 2016 Maksym Savisko. All rights reserved.
+//
+
+#import "MSStringsResultVC.h"
+
+@implementation MSStringsResultVC
+
+@end
