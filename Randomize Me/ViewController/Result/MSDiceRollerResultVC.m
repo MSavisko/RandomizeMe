5e319150383632824c8d9df52fa05b181069295f
Maksym Savisko <stowyn@gmail.com>
2016-04-13 23:10:36 +0300

    1. Presentation Data Method for dice roller

--- Randomize Me/ViewController/Result/MSDiceRollerResultVC.m
+++ Randomize Me/ViewController/Result/MSDiceRollerResultVC.m
@@ -57,11 +57,11 @@
     [[VKSdk instance] setUiDelegate:self];
 }
 
-#pragma mark - Presentation Data Method
 - (void) setArrayImage {
     self.arrayOfImage = [NSArray arrayWithObjects:self.firstImageView, self.secondImageView, self.thirdImageView, self.fourthImageView, self.fifthImageView, self.sixthImageView, nil];
 }
 
+#pragma mark - Presentation Image Method
 - (NSString*) imageNameFromeDice:(NSNumber*)number {
     int diceNumber = [number intValue];
     switch (diceNumber) {
@@ -97,6 +97,35 @@
         [self.arrayOfImage[i] setImage:image];
     }
 }
+
+#pragma mark - Presentation Data Method
+- (NSString*) stringComplitionTime {
+    return [self.response.completionTime substringToIndex:self.response.completionTime.length-1];
+}
+
+- (NSString*) stringResult {
+    NSString *result = [[self.response.data valueForKey:@"description"] componentsJoinedByString:@" dice"];
+    NSMutableString *mutableResult = [NSMutableString stringWithFormat:@"dice%@", result];
+    return mutableResult;
+}
+
+- (NSString*) stringResultForCopying {
+    NSString *resultName = @"Dice Roller Generation";
+    NSString *forResult = @"Result:";
+    NSString *resultData = [self stringResult];
+    NSString *parametrs = @"Parameters of generation:";
+    NSString *numberOfStrings = [NSString stringWithFormat:@"Number of Dice: %d", self.response.data.count];
+    NSString *replacement = @"Unique Dice: NO";
+    NSString *individualInformation = @"Individual information of generation:";
+    NSString *completionTime = [NSString stringWithFormat:@"Completion time (UTC+0): %@", [self stringComplitionTime]];
+    NSString *serialNumber = [NSString stringWithFormat:@"Serial Number: %ld", (long)self.response.serialNumber];
+    NSString *result = [NSString stringWithFormat:@"%@\n\n%@\n%@\n\n%@\n%@\n%@\n\n%@\n%@\n%@", resultName, forResult, resultData, parametrs, numberOfStrings, replacement, individualInformation, completionTime, serialNumber];
+    return result;
+}
+
+//- (NSString*) stringResultForShare {
+//    return result;
+//}
 
 
 
