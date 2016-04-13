1b5691f6bda96cf5f8d946839406a55fa42b07da
Maksym Savisko <stowyn@gmail.com>
2016-04-13 21:08:52 +0300

    Init Dice Roller VC

--- Randomize Me/ViewController/Result/MSDiceRollerResultVC.m
+++ Randomize Me/ViewController/Result/MSDiceRollerResultVC.m
@@ -7,6 +7,24 @@
 //
 
 #import "MSDiceRollerResultVC.h"
+#import "SWRevealViewController.h"
+
+#import "MBProgressHUD.h"
+
+#import <Social/Social.h>
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKShareKit/FBSDKShareKit.h>
+#import <VK-ios-sdk/VKSdk.h>
+
+@interface MSDiceRollerResultVC () <UIActionSheetDelegate, UIAlertViewDelegate, VKSdkUIDelegate>
+@property (weak, nonatomic) IBOutlet UIImageView *firstImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *secondImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *thirdImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *fourthImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *fifthImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *sixthImageView;
+
+@end
 
 @implementation MSDiceRollerResultVC
 
