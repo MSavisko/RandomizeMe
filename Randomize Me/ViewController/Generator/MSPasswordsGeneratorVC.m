9dede583ce2f80ae62eaac386902a8faed3aa14c
Maksym Savisko <stowyn@gmail.com>
2016-04-13 10:20:18 +0300

    init Passwords Generator

---
+++ Randomize Me/ViewController/Generator/MSPasswordsGeneratorVC.m
@@ -0,0 +1,13 @@
+//
+//  MSPasswordsGeneratorVC.m
+//  Randomize Me
+//
+//  Created by Maksym Savisko on 4/13/16.
+//  Copyright © 2016 Maksym Savisko. All rights reserved.
+//
+
+#import "MSPasswordsGeneratorVC.h"
+
+@implementation MSPasswordsGeneratorVC
+
+@end
