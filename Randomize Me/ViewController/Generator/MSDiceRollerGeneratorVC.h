383d428bd0d2ca88c3d9db0dace2bd126e4db021
Maksym Savisko <stowyn@gmail.com>
2016-04-13 16:43:41 +0300

    Init Dice Roller VC

---
+++ Randomize Me/ViewController/Generator/MSDiceRollerGeneratorVC.h
@@ -0,0 +1,13 @@
+//
+//  MSDiceRollerGeneratorVC.h
+//  Randomize Me
+//
+//  Created by Maksym Savisko on 4/13/16.
+//  Copyright © 2016 Maksym Savisko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSDiceRollerGeneratorVC : UIViewController
+
+@end
---
+++ Randomize Me/ViewController/Generator/MSDiceRollerGeneratorVC.m
@@ -0,0 +1,17 @@
+//
+//  MSDiceRollerGeneratorVC.m
+//  Randomize Me
+//
+//  Created by Maksym Savisko on 4/13/16.
+//  Copyright © 2016 Maksym Savisko. All rights reserved.
+//
+
+#import "MSDiceRollerGeneratorVC.h"
+
+@interface MSDiceRollerGeneratorVC ()
+
+@end
+
+@implementation MSDiceRollerGeneratorVC
+
+@end
