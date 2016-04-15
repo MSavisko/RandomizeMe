6be87af00817a883d9f485ceeb6fb3b007ffe7d6
Maksym Savisko <stowyn@gmail.com>
2016-04-15 13:21:05 +0300

    Init List Randomiser

---
+++ Randomize Me/ViewController/Generator/MSListRandomizerVC.h
@@ -0,0 +1,13 @@
+//
+//  MSListRandomizerVC.h
+//  Randomize Me
+//
+//  Created by Maksym Savisko on 4/15/16.
+//  Copyright © 2016 Maksym Savisko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSListRandomizerVC : UIViewController
+
+@end
---
+++ Randomize Me/ViewController/Generator/MSListRandomizerVC.m
@@ -0,0 +1,13 @@
+//
+//  MSListRandomizerVC.m
+//  Randomize Me
+//
+//  Created by Maksym Savisko on 4/15/16.
+//  Copyright © 2016 Maksym Savisko. All rights reserved.
+//
+
+#import "MSListRandomizerVC.h"
+
+@implementation MSListRandomizerVC
+
+@end
