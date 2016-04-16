8431f6bb2f1f66835463dd13b48926c553ee0eaf
Maksym Savisko <stowyn@gmail.com>
2016-04-16 22:38:15 +0300

    Init lottery quick pick VC

---
+++ Randomize Me/ViewController/Generator/MSLoteryQuickPickVC.h
@@ -0,0 +1,13 @@
+//
+//  MSLoteryTicketGenerator.h
+//  Randomize Me
+//
+//  Created by Maksym Savisko on 4/16/16.
+//  Copyright © 2016 Maksym Savisko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSLoteryQuickPickVC : UIViewController
+
+@end
---
+++ Randomize Me/ViewController/Generator/MSLoteryQuickPickVC.m
@@ -0,0 +1,13 @@
+//
+//  MSLoteryTicketGenerator.m
+//  Randomize Me
+//
+//  Created by Maksym Savisko on 4/16/16.
+//  Copyright © 2016 Maksym Savisko. All rights reserved.
+//
+
+#import "MSLoteryQuickPickVC.h"
+
+@implementation MSLoteryQuickPickVC
+
+@end
