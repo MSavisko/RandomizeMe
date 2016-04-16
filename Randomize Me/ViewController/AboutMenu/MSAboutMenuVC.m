a2e10ff8ae32e7e28ccf71d8b4b004231a9bfc7a
Maksym Savisko <stowyn@gmail.com>
2016-04-16 13:49:06 +0300

    Send Feedback - added

--- Randomize Me/ViewController/AboutMenu/MSAboutMenuVC.m
+++ Randomize Me/ViewController/AboutMenu/MSAboutMenuVC.m
@@ -8,8 +8,10 @@
 
 #import "MSAboutMenuVC.h"
 #import "SWRevealViewController.h"
+#import <MessageUI/MessageUI.h>
+#import "MBProgressHUD.h"
 
-@interface MSAboutMenuVC ()
+@interface MSAboutMenuVC () <MFMailComposeViewControllerDelegate>
 @property (nonatomic, strong) NSArray *menuItems;
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *menuButtonItem;
 
@@ -43,7 +45,7 @@
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     if ([self.menuItems[indexPath.row]isEqualToString:@"feedback"]) {
-       //Send Feedback
+        [self sendFeedback];
     }
     else if ([self.menuItems[indexPath.row]isEqualToString:@"license"]) {
         //Segue with idenifier License
@@ -55,8 +57,30 @@
     
 }
 
+#pragma mark - Mail View Controller Delegate
+- (void) mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error
+{
+    [self dismissViewControllerAnimated:YES completion:NULL];
+    switch (result)
+    {
+        case MFMailComposeResultCancelled:
+            [self showHudWithMessage:@"Cancelled"];
+            break;
+        case MFMailComposeResultSaved:
+            [self showHudWithMessage:@"Saved"];
+            break;
+        case MFMailComposeResultSent:
+            [self showHudWithMessage:@"Feedback sent"];
+            break;
+        case MFMailComposeResultFailed:
+            [self showFeedbackAlert];
+            break;
+        default:
+            break;
+    }
+}
 
-#pragma mark - Table View data source
+#pragma mark - Table View Data source
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     // Return the number of sections.
     return 1;
@@ -82,8 +106,7 @@
 
 //**Because of warning in 8.1
 //**"Warning once only: Detected a case where constraints ambiguously suggest a height of zero for a tableview cell's content view. We're considering the collapse unintentional and using standard height instead.
-
-#pragma mark - Table View Cell data source
+#pragma mark - Table View Cell Data source
 - (CGFloat)tableView:(UITableView *)tableView
 estimatedHeightForRowAtIndexPath:(NSIndexPath *)indexPath {
     return 44;
@@ -92,6 +115,44 @@ estimatedHeightForRowAtIndexPath:(NSIndexPath *)indexPath {
 - (CGFloat)tableView:(UITableView *)tableView
 heightForRowAtIndexPath:(NSIndexPath *)indexPath {
     return 44;
+}
+
+#pragma mark - Helper Method
+- (void) sendFeedback {
+    NSString *emailTitle = @"Feedback for Randomize Me";
+    NSString *messageBody = @"";
+    NSArray *toRecipents = [NSArray arrayWithObject:@"stowyn@gmail.com"];
+    MFMailComposeViewController *mc = [[MFMailComposeViewController alloc] init];
+    mc.mailComposeDelegate = self;
+    [mc setSubject:emailTitle];
+    [mc setMessageBody:messageBody isHTML:YES];
+    [mc setToRecipients:toRecipents];
+    [self presentViewController:mc animated:YES completion:NULL];
+}
+
+#pragma mark - MBProgressHUD Method
+- (void) showHudWithMessage:(NSString*)message {
+    //Show
+    MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    hud.mode = MBProgressHUDModeCustomView;
+    hud.labelText = message;
+    
+    //Hide
+    dispatch_async(dispatch_get_main_queue(), ^{
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.7 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [MBProgressHUD hideAllHUDsForView:self.view animated:YES];
+        });
+    });
+}
+
+#pragma mark - Alert Methods
+- (void) showFeedbackAlert {
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Feedback!"
+                                                    message:@"Sorry. Could not send feedback.\nTry again later."
+                                                   delegate:self
+                                          cancelButtonTitle:@"OK"
+                                          otherButtonTitles:nil];
+    [alert show];
 }
 
 @end
